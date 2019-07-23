cb1cb5021303220627bf0b896fad1ce27cef59ac
ChimeraRonin <50640460+ChimeraRonin@users.noreply.github.com>
2019-07-23 16:01:39 -0400

    Files added to change the name of the repo.
    Repo cannot show Code Louisville Project.

---
+++ README.md
@@ -0,0 +1,14 @@
+# CodeLouisvilleProject
+Code Louisville Project
+
+My project is to try to replicate a simple restaurant site. It consists of the following:
+
+*Home page <br>
+*Menu page <br>
+*locations page <br>
+*JavaScript for the carousel in carscript.js <br>
+*JavaScript for the Reservations modal function for in script.js <br>
+
+It is painfully simple, and I still needed and received a good bit of help, for which I am very grateful. Before Code Louisville, I knew nothing about coding. Looking at my site, that may still appear to be the case.
+
+I have learned a lot, and I intend to continue learning now that some of the sorcery behind coding has been slightly demystified.
---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('.form');
+const reservationBtn = document.getElementById('jf-button-reservation');
+const reservationModal = document.getElementById("reservations");
+const blurModal = document.getElementById("blur-modal");
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  alert("See you then!");
+  console.log(event);
+  closeModal()
+});
+
+// Begin Modal Window for Reservations 
+reservationBtn.addEventListener('click', openModal)
+
+
+// Function to blur background when modal is open
+
+function openModal() {
+  reservationModal.classList.add("jf-modal-container-show");
+  blurModal.classList.add("blur-show");
+}
+
+function closeModal() {
+  reservationModal.classList.remove("jf-modal-container-show");
+  blurModal.classList.remove("blur-show");
+}
+
+// End Modal Window for Reservations 
+
